463cc1d6cbdc5cf08028543b98bb9a4753f6f126
hanzn-zzx <hanznzzx@126.com>
2024-12-21 20:05:13 +0800

    11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/e9d015c9/","posts/6f4302ac/","posts/ff05b5bf/","posts/9876ecb4/","posts/2b9cc7eb/","posts/9f50842e/","posts/ba1d0753/","posts/875648f9/"];function toRandomPost(){
+var posts=["posts/ff05b5bf/","posts/6f4302ac/","posts/e9d015c9/","posts/9f50842e/","posts/875648f9/","posts/9876ecb4/","posts/ba1d0753/","posts/2b9cc7eb/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
